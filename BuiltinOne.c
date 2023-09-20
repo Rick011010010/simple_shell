a1acb4e885c45b01acf7f743f5e012ce919fbfbd
elmyar ossama <oussama.elmyar92@gmail.com>
2023-09-20 04:16:27 -0700

    origin2

--- BuiltinOne.c
+++ BuiltinOne.c
@@ -9,8 +9,8 @@
  */
 int Myhtr(info_t *info)
 {
-    print_list(info->history);
-    return (0);
+	print_list(info->history);
+	return (0);
 }
 
 /**
@@ -21,18 +21,18 @@ int Myhtr(info_t *info)
  */
 int unset_alias(info_t *info, char *str)
 {
-    char *equal_sign, saved_char;
-    int ret;
+	char *equal_sign, saved_char;
+	int ret;
 
-    equal_sign = _strchr(str, '=');
-    if (!equal_sign)
-        return (1);
-    saved_char = *equal_sign;
-    *equal_sign = '\0';
-    ret = delete_node_at_index(&(info->alias),
-                               get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-    *equal_sign = saved_char;
-    return (ret);
+	equal_sign = _strchr(str, '=');
+	if (!equal_sign)
+		return (1);
+	saved_char = *equal_sign;
+	*equal_sign = '\0';
+	ret = delete_node_at_index(&(info->alias),
+							   get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*equal_sign = saved_char;
+	return (ret);
 }
 
 /**
@@ -43,16 +43,16 @@ int unset_alias(info_t *info, char *str)
  */
 int set_alias(info_t *info, char *str)
 {
-    char *equal_sign;
+	char *equal_sign;
 
-    equal_sign = _strchr(str, '=');
-    if (!equal_sign)
-        return (1);
-    if (!*++equal_sign)
-        return (unset_alias(info, str));
+	equal_sign = _strchr(str, '=');
+	if (!equal_sign)
+		return (1);
+	if (!*++equal_sign)
+		return (unset_alias(info, str));
 
-    unset_alias(info, str);
-    return (add_node_end(&(info->alias), str, 0) == NULL);
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
@@ -62,19 +62,19 @@ int set_alias(info_t *info, char *str)
  */
 int print_alias(list_t *node)
 {
-    char *equal_sign, *alias;
+	char *equal_sign, *alias;
 
-    if (node)
-    {
-        equal_sign = _strchr(node->str, '=');
-        for (alias = node->str; alias <= equal_sign; alias++)
-            _putchar(*alias);
-        _putchar('\'');
-        _puts(equal_sign + 1);
-        _puts("'\n");
-        return (0);
-    }
-    return (1);
+	if (node)
+	{
+		equal_sign = _strchr(node->str, '=');
+		for (alias = node->str; alias <= equal_sign; alias++)
+			_putchar(*alias);
+		_putchar('\'');
+		_puts(equal_sign + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
 }
 
 /**
@@ -85,28 +85,29 @@ int print_alias(list_t *node)
  */
 int Myalias(info_t *info)
 {
-    int i = 0;
-    char *equal_sign, *p = NULL;
-    list_t *node = NULL;
+	int i = 0;
+	char *equal_sign, *p = NULL;
+	list_t *node = NULL;
 
-    if (info->argc == 1)
-    {
-        node = info->alias;
-        while (node)
-        {
-            print_alias(node);
-            node = node->next;
-        }
-        return (0);
-    }
-    for (i = 1; info->argv[i]; i++)
-    {
-        equal_sign = _strchr(info->argv[i], '=');
-        if (equal_sign)
-            set_alias(info, info->argv[i]);
-        else
-            print_alias(node_starts_with(info->alias, info->argv[i], '='));
-    }
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		equal_sign = _strchr(info->argv[i], '=');
+		if (equal_sign)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
 
-    return (0);
+	return (0);
 }
+
