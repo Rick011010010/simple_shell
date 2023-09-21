4b3df7dbf58c56cae34d6f22802b13cf70009a7f
elmyar ossama <oussama.elmyar92@gmail.com>
2023-09-20 17:28:49 -0700

    s

--- header.c
+++ header.c
@@ -15,3 +15,28 @@ int main(int argc, char **argv)
 
 	return (0);
 }
+
+/**
+ * create_cd - handle the command cd
+ * @t: data
+ */
+void create_cd(data *t)
+{
+	char *d = t->argv[1];
+	char cwd[256];
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		perror("getcwd() error");
+	_setenv(t, "PWD", cwd);
+	if (!d)
+		d = _getenv("HOME");
+	if (chdir(d) == -1)
+		perror("cd");
+	else
+	{
+		_setenv(t, "OLDPWD", _getenv("PWD"));
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			perror("getcwd() error");
+		_setenv(t, "PWD", cwd);
+	}
+}
